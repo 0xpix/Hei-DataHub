9bf9e0781e05333aea9fd763a76a7e80c30d7113
Aness Chelfat <ns.chlfat@gmail.com>
2025-10-04 08:28:56 +0200

    feat(version): add version information and GitHub update check URL

---
+++ mini_datahub/version.py
@@ -0,0 +1,10 @@
+"""
+Version information for Mini Hei-DataHub.
+"""
+
+__version__ = "3.0.0"
+__app_name__ = "Mini Hei-DataHub"
+
+# GitHub repository for update checking
+GITHUB_REPO = "0xpix/Hei-DataHub"
+UPDATE_CHECK_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
